f3ccdf3af3e63ca121e2ff4ffc24b65d3274ec58
p3160121 <cosatsxusa@gmail.com>
2019-12-05 14:55:22 +0200

    Changes

--- tens.py
+++ tens.py
@@ -8,7 +8,7 @@ from Reader import *
 
 def sequential_nn_model(X_train, y_train):
     model = Sequential([
-        Dense(100, activation='relu', input_shape=(56,)),
+        Dense(100, activation='relu', input_shape=(57,)),
         Dense(40, activation='relu'),
         Dense(20, activation='relu'),
         Dense(1, activation='relu')
@@ -20,64 +20,65 @@ def sequential_nn_model(X_train, y_train):
     hist = model.fit(X_train, y_train, epochs=100)
     return model
 
+if __name__=='__main__':
 
-pd.set_option('display.max_columns', 100)
-pd.set_option('display.max_rows', 13000)
+    pd.set_option('display.max_columns', 100)
+    pd.set_option('display.max_rows', 13000)
 
-filename = 'train.csv'
-df_train = read_data(filename)
+    filename = 'train.csv'
+    df_train = read_data(filename)
 
-df_test = read_data('test.csv')  # reading test file
+    df_test = read_data('test.csv')  # reading test file
 
-# Training
-all_columns = list(df_train.columns)
-# Training and test data is created by splitting the main data. 30% of test data is considered
-train_columns = ['season', 'month', 'hour', 'holiday', 'weekday', 'workingday', 'weather', 'temp', 'humidity']
-X = df_train[[x for x in all_columns if x.startswith(tuple(train_columns))]]  # getting all desired
-# print(X)
-X = X.drop(columns=['weather_4'])
-print(X.columns)
-y = df_train['count']
+    # Training
+    all_columns = list(df_train.columns)
+    # Training and test data is created by splitting the main data. 30% of test data is considered
+    train_columns = ['season', 'month', 'hour', 'holiday', 'weekday', 'workingday', 'weather', 'temp', 'humidity']
+    X = df_train[[x for x in all_columns if x.startswith(tuple(train_columns))]]  # getting all desired
+    # print(X)
+    X = X.drop(columns=['weather_4'])
+    print(X.columns)
+    y = df_train['count']
 
-# Creating the split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # Creating the split
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-# Initializing MLPRegressor
-neural = MLPRegressor(hidden_layer_sizes=(100, 60, 40, 20), activation='relu', solver='lbfgs', alpha=0.0001,
-                      verbose=True)
-neural.fit(X_train, y_train)
+    # Initializing MLPRegressor
+    neural = MLPRegressor(hidden_layer_sizes=(100, 60, 40, 20), activation='relu', solver='lbfgs', alpha=0.0001,
+                          verbose=True)
+    neural.fit(X_train, y_train)
 
-_, test_acc = neural.evaluate(X_test, y_test, verbose=0)  # evaluating MLPRegressor
-print('Test: %.3f' % test_acc)
+    _, test_acc = neural.evaluate(X_test, y_test, verbose=0)  # evaluating MLPRegressor
+    print('Test: %.3f' % test_acc)
 
-# Initializing Sequential NN
-model = sequential_nn_model(X_train, y_train)
+    # Initializing Sequential NN
+    model = sequential_nn_model(X_train, y_train)
 
-df_test['weather_4'] = 0
-df_test = df_test[[x for x in all_columns if x.startswith(tuple(train_columns))]]
-df_test = df_test.drop(columns=['weather_4'])
-print(df_test.columns)
-test_array = df_test.to_numpy()
-predictions = model.predict(test_array)
-individual_predictions = [transform_list(x) for x in predictions]
-for i, y in enumerate(individual_predictions):
-    if individual_predictions[i] < 0:
-        individual_predictions[i] = 0
+    df_test['weather_4'] = 0
+    df_test = df_test[[x for x in all_columns if x.startswith(tuple(train_columns))]]
+    df_test = df_test.drop(columns=['weather_4'])
+    print(df_test.columns)
+    test_array = df_test.to_numpy()
+    predictions = model.predict(test_array)
+    individual_predictions = [transform_list(x) for x in predictions]
+    for i, y in enumerate(individual_predictions):
+        if individual_predictions[i] < 0:
+            individual_predictions[i] = 0
 
-# mlp_predictions=neural.predict(df_test_og)
-# for i, y in enumerate(mlp_predictions):
-#     if mlp_predictions[i] < 0:
-#         mlp_predictions[i] = 0
+    # mlp_predictions=neural.predict(df_test_og)
+    # for i, y in enumerate(mlp_predictions):
+    #     if mlp_predictions[i] < 0:
+    #         mlp_predictions[i] = 0
 
-# full_set = dstack((individual_predictions,mlp_predictions))
-# full_set = full_set.reshape(full_set.shape[0],full_set.shape[1]*full_set.shape[2])
-# print(full_set.shape)
-# ensemble_model = LogisticRegression()
-# ensemble_model.fit(full_set, y)
-#
-# final_prediction = ensemble_model.predict(test_array)
+    # full_set = dstack((individual_predictions,mlp_predictions))
+    # full_set = full_set.reshape(full_set.shape[0],full_set.shape[1]*full_set.shape[2])
+    # print(full_set.shape)
+    # ensemble_model = LogisticRegression()
+    # ensemble_model.fit(full_set, y)
+    #
+    # final_prediction = ensemble_model.predict(test_array)
 
-submission = pd.DataFrame()
-submission['Id'] = range(len(individual_predictions))
-submission['Predicted'] = individual_predictions
-submission.to_csv("submission.csv", index=False)
+    submission = pd.DataFrame()
+    submission['Id'] = range(len(individual_predictions))
+    submission['Predicted'] = individual_predictions
+    submission.to_csv("submission.csv", index=False)
--- test.py
+++ test.py
@@ -8,65 +8,56 @@ from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPRegressor
 import tensorflow as tf
 from Reader import *
+from tens import sequential_nn_model
 
 
 pd.set_option('display.max_columns', 100)
 pd.set_option('display.max_rows', 13000)
 
 
-
-
-
 filename = 'train.csv'
 df_train = read_data(filename)
-# print(df_train.head(10))
-
 df_test = read_data('test.csv')  # reading test file
-# print(df_test.head(10))
 
-# print(df_train.head(10))
-# print(df_train.head(10))
 # Training
 all_columns = list(df_train.columns)
 # Training and test data is created by splitting the main data. 30% of test data is considered
 train_columns = ['season', 'month', 'hour', 'holiday', 'weekday', 'workingday', 'weather', 'temp', 'humidity']
 X = df_train[[x for x in all_columns if x.startswith(tuple(train_columns))]]  # getting all desired
-# print(X)
+
 y = df_train['count']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 
-reg = tf.estimator.LinearClassifier(X)
-reg.train(X_train, max_steps=100)
-result = reg.evaluate(X_test)
-print(pd.Series(result))
-#print(reg.score(X_test, y_test))
 
-n_batches = 1
-est = tf.estimator.BoostedTreesClassifier(X, n_batches_per_layer=n_batches)
-est.train(X_train, max_steps=100)
-result2=est.evaluate(X_test)
-print(pd.Series(result2))
+#Tensorflow Sequential Neural Network
+Seq_NN = sequential_nn_model(X_train, y_train)
 
-y_pred = reg.predict(X_test)
-# print(y_pred)
-for i, y in enumerate(y_pred):
-    if y_pred[i] < 0:
-        y_pred[i] = 0
+df_test['weather_4'] = 0
+df_test = df_test[[x for x in all_columns if x.startswith(tuple(train_columns))]]
+df_test = df_test.drop(columns=['weather_4'])
 
-print('RMSLE:', np.sqrt(mean_squared_log_error(y_test, y_pred)))
-print('R2:', r2_score(y_test, y_pred))
+test_array=df_test.to_numpy()
+predictions=Seq_NN.predict(test_array)
+
+individual_predictions = [transform_list(x) for x in predictions]
+for i, y in enumerate(individual_predictions):
+    if individual_predictions[i] < 0:
+        individual_predictions[i] = 0
+
+print('RMSLE:', np.sqrt(mean_squared_log_error(y_test, individual_predictions)))
+print('R2:', r2_score(y_test, individual_predictions))
 
 # reg_bag = BaggingRegressor(reg)
 # reg_bag.fit(X, y)
-df_test['weather_4'] = 0
-df_test = df_test[[x for x in all_columns if x.startswith(tuple(train_columns))]]  # getting all desired
-y_pred = est.predict(df_test)
-for i, y in enumerate(y_pred):
-    if y_pred[i] < 0:
-        y_pred[i] = 0
+# df_test['weather_4'] = 0
+# df_test = df_test[[x for x in all_columns if x.startswith(tuple(train_columns))]]  # getting all desired
+# y_pred = est.predict(df_test)
+# for i, y in enumerate(y_pred):
+#     if y_pred[i] < 0:
+#         y_pred[i] = 0
 
 submission = pd.DataFrame()
-submission['Id'] = range(y_pred.shape[0])
-submission['Predicted'] = y_pred
+submission['Id'] = range(individual_predictions.shape[0])
+submission['Predicted'] = individual_predictions
 submission.to_csv("submission.csv", index=False)
